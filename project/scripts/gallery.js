81081d08e3d819b98d9263cf6da039614b71e6ed
bader911 <47529701+bader911@users.noreply.github.com>
2019-05-02 18:28:40 +0100

    Add files via upload

---
+++ project/scripts/gallery.js
@@ -0,0 +1,21 @@
+﻿var displayedImage = document.querySelector('.displayed-img');
+var thumbBar = document.querySelector('.thumb-bar');
+
+btn = document.querySelector('button');
+var overlay = document.querySelector('.overlay');
+
+/* Looping through images */
+
+for(var i = 1; i <= 12; i++) {
+  var newImage = document.createElement('img');
+  newImage.setAttribute('src', 'images/pic' + i + '.jpg');
+  thumbBar.appendChild(newImage);
+  newImage.onclick = function(e) {
+    var imgSrc = e.target.getAttribute('src');
+    displayImage(imgSrc);
+  }
+}
+
+function displayImage(imgSrc) {
+  displayedImage.setAttribute('src', imgSrc);
+}
